fc2ca8ee57a4788baf06d5e14228e806f21afdbe
Галина <g416@mail.ru>
2023-06-05 23:09:52 +0500

    Добро пожаловать  корректировка

--- src/main/java/prosky/galkacalculator/HelloController.java
+++ src/main/java/prosky/galkacalculator/HelloController.java
@@ -1,15 +1,18 @@
 package prosky.galkacalculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping
-
+    @RequestMapping("/calculator")
     public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
+
+
+
 
 
 }
